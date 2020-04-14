24da7e2578753b4d531e7b906180fd4ee7697583
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-14 18:56:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do 
    @repeat = params[:number].to_i
   @repeat
-  @phrase = params[:phrase].to_s
+  @phrase = params[:phrase]
   @repeat
  
      
