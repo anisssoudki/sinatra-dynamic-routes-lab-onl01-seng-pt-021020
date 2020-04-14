09f5280d59fc32a8208fa1de9948ef70a432a744
Aniss Marrouch <aniss.marrouch@gmail.com>
2020-04-14 20:15:39 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,21 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require './app'
+# ActiveRecord::Base.establish_connection(
+#   :adapter   =>   'sqlite3'
+#   :database  =>   "db/#{ENV['SINATRA_ENV']}.sqlite"
+#   )
+#   require_relative "../app.rb"
+
+require "./app"
+# sql = <<-SQL
+#   CREATE TABLE IF NOT EXISTS users (
+#   id INTEGER PRIMARY KEY,
+#   name TEXT,
+#   genre TEXT,
+#   age INTEGER,
+#   hometown TEXT
+#   )
+# SQL
+ 
+# ActiveRecord::Base.connection.execute(sql)
